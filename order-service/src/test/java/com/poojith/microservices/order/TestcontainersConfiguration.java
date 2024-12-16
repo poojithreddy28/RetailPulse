b3ebd9b6ad126660868aa0f7dbf1264865d05a7f
poojithreddy28 <poojithannachedu@gmail.com>
2024-12-15 22:35:04 -0600

    initial commit : api-gateway

--- order-service/src/test/java/com/poojith/microservices/order/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.poojith.microservices.order;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
