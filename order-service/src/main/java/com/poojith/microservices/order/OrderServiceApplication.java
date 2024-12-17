2940d215a6f8919dd41b21fa27cfe418223e4464
poojithreddy28 <poojithannachedu@gmail.com>
2024-12-17 00:20:10 -0600

    refactor(order-service): Replace OpenFeign client with RestClient for future compatibility and improved maintainability

--- order-service/src/main/java/com/poojith/microservices/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/poojith/microservices/order/OrderServiceApplication.java
@@ -2,10 +2,9 @@ package com.poojith.microservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
-@EnableFeignClients	// Enable Feign Clients
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
