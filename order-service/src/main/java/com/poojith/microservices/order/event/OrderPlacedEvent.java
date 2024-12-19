350c8e8520ed361a065e476b6966bbe95605edb6
poojithreddy28 <poojithannachedu@gmail.com>
2024-12-19 11:34:29 -0600

    Add constructors, getters and setters to OrderPlacedEvent model

--- order-service/src/main/java/com/poojith/microservices/order/event/OrderPlacedEvent.java
+++ order-service/src/main/java/com/poojith/microservices/order/event/OrderPlacedEvent.java
@@ -5,14 +5,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+
 @Data
 public class OrderPlacedEvent {
     private String orderNumber;
     private String email;
 
+    public OrderPlacedEvent() {
+    }
+
     public OrderPlacedEvent(String orderNumber, String email) {
         this.orderNumber = orderNumber;
+        this.email = email;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 }
